1b3ddf360279c1c0bcc1f9e8c28ae2484b7be1ce
msgyankee <msgyankee@gmail.com>
2020-04-16 16:07:12 -0400

    Added preprocessing for NYT Data

--- resources/all.php
+++ resources/all.php
@@ -5,7 +5,96 @@ v.3.0 (2017) -- now supports Google Maps
 
 
 */
+
 var stop =[]; //Global variable updated from data load that holds the dynamic stop date for Timelines
+/// START CUSTOM PREPROCESSING FUNCTIONS
+
+const indexMap = {
+    'Alabama': 0,
+    'Alaska': 1,
+    'Arizona': 2,
+    'Arkansas': 3,
+    'California': 4,
+    'Colorado': 5,
+    'Connecticut': 6,
+    'Delaware': 7,
+    'District of Columbia': 8,
+    'Florida': 9,
+    'Georgia': 10,
+    'Hawaii': 11,
+    'Idaho': 12,
+    'Illinois': 13,
+    'Indiana': 14,
+    'Iowa': 15,
+    'Kansas': 16,
+    'Kentucky': 17,
+    'Louisiana': 18,
+    'Maine': 19,
+    'Maryland': 20,
+    'Massachusetts': 21,
+    'Michigan': 22,
+    'Minnesota': 23,
+    'Mississippi': 24,
+    'Missouri': 25,
+    'Montana': 26,
+    'Nebraska': 27,
+    'Nevada': 28,
+    'New Hampshire': 29,
+    'New Jersey': 30,
+    'New Mexico': 31,
+    'New York': 32,
+    'North Carolina': 33,
+    'North Dakota': 34,
+    'Ohio': 35,
+    'Oklahoma': 36,
+    'Oregon': 37,
+    'Pennsylvania': 38,
+    'Rhode Island': 39,
+    'South Carolina': 40,
+    'South Dakota': 41,
+    'Tennessee': 42,
+    'Texas': 43,
+    'Utah': 44,
+    'Vermont': 45,
+    'Virginia': 46,
+    'Washington': 47,
+    'West Virginia': 48,
+    'Wisconsin': 49,
+    'Wyoming': 50
+}
+
+function start(data){
+    var results = [];
+    for (var state in indexMap){
+        results.push({'state' : state});
+    }
+
+    data.forEach(function (entry, index){
+        var datec = entry.date + 'c';
+        var dated = entry.date + 'd';
+
+        if(!(datec in results[0])){
+            results.forEach(function (row, index){
+                row[datec] = 0;
+                row[dated] = 0;
+            });
+        }
+        if(entry.state != "American Samoa" && entry.state != "Guam" && entry.state != "Northern Mariana Islands" && entry.state != "Puerto Rico" && entry.state != "Virgin Islands"){
+			results[indexMap[entry.state]][datec] = entry.cases;
+            results[indexMap[entry.state]][dated] = entry.deaths;    
+        }
+    });
+    return results;
+}
+
+function latest(results){
+    var keys = Object.keys(results[0]);
+    var split = keys[keys.length-1].split("-");
+    return [parseInt(split[0]), parseInt(split[1]), parseInt(split[2].substring(0,split[2].length-1))];
+}
+
+
+///END PREPROCESSING FUNCTIONS
 
 var d3VizObj = {
 	//holds data objects
@@ -153,10 +242,12 @@ function d3Data(options) {
             obj.filetype = "CSV";
             d3.csv(this.csvext, function(error, data){   //Asynchronously retrive csv, already in array form. Assign latest date to the stop var. 
                     if(error) console.log(error);
-                    else{ 
-						stop = [2020,4,7]
-                        obj.data = data;
-                        console.log(obj);
+                    else{ //Data successfully retrieved
+
+						results = start(data); //Preprocesses the data to a usable format
+						stop = latest(results); //Retrieves latest date recieved during preprocessing
+
+                        obj.data = results;
                         d3VizObj.runHook("run_after_data_loaded",obj);
                         d3VizObj.is_loaded(obj);
                     } 
@@ -1091,7 +1182,6 @@ function Timeline(options) {
 	if(Array.isArray(this.stopDate) && this.stopDate.length == 0) this.stopDate = stop;
 			
 	this.currentDate = this.stopDate;
-	console.log(this.currentDate);
 	this.lastDate = [];
 	this.index = [];
 	
